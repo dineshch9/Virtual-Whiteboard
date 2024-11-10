2ece6e6bff4a4034b0ee8d9e18ca6bbf1ee0a80c
dineshch9 <dineshch489@gmail.com>
2024-11-10 16:07:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,11 @@ import Toolbox from "./components/Toolbox";
 import BoardProvider from "./store/BoardProvider";
 import ToolboxProvider from "./store/ToolboxProvider";
 import Card from "./components/card/Card";
-import { useState } from "react";
-
-const x=false;
-
 
 
 
 function App() {
-  // const x=useState(window.innerWidth);
+
   if(!(/Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent))){
     return (
       <>
